f41001e9da4b2271957a4b01edbaed55931e1d7b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-21 15:26:57 -0700

    Cache is hard, I suppose

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-( cd resources \
-  && npm install \
-  && npm exec slidev build --base "/emberconf-2023-resources/" \
-)
-
 rm -rf docs
-mv resources/dist docs
 
+cd resources \
+&& npm install \
+&& npm exec slidev -- build \
+  --base="/emberconf-2023-resources/" \
+  --out="../docs/"
